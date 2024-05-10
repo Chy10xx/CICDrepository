1473d5d7c61f09d98fc8e602c9e1c4ae99d3834f
Chy10xx <167799430+Chy10xx@users.noreply.github.com>
2024-05-09 22:38:58 -0600

    Update OrderDao.java

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -22,7 +22,7 @@ public class OrderDao {
                 new Order(113, "Headphones", 2, 500),
                 new Order(114, "Refrigerator", 5, 2500),
                 new Order(115, "Play station", 3, 1500),
-                new Order(116, "Scarf", 2, 500),
+                new Order(116, "Scarf", 2, 500))
                 .collect(Collectors.toList());
     }
 }
